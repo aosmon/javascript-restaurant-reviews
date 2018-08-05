2c0ebf99ca9591974ad78dbc403ec68ebdfa539b
Aizhan Osmonova <osmonova86@gmail.com>
2018-08-05 11:27:54 -0400

    fix: Accessibility - add alt for restaurant images

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', restaurant.photo_alt);
   li.append(image);
 
   const name = document.createElement('h3');
