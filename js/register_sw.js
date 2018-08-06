a9b4873fe8ad5c2cb1f52b9ee8c85c73ea660412
Aizhan Osmonova <osmonova86@gmail.com>
2018-08-05 22:00:35 -0400

    fix: Remove unused code

--- js/register_sw.js
+++ js/register_sw.js
@@ -1,15 +1,4 @@
   navigator.serviceWorker.register('/js/sw.js').then(function(reg) {
 
     console.log('Service worker successfully registered', reg);
-    
-    if (reg.waiting) {
-      return;
-    }
-
-    if (reg.installing) {
-      return;
-    }
-
-  }, function(err) {
-      console.log(err);
 });
